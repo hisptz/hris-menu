d2b618a494ff3ef232d31605914619defdf1b2a8
Rajab Mkomwa <rajeyw@gmail.com>
2019-09-23 12:27:04 +0300

    feat: create menu template

--- projects/hris-menu/src/lib/hris-menu.module.ts
+++ projects/hris-menu/src/lib/hris-menu.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { HrisMenuComponent } from './containers/hris-menu/hris-menu.component';
 import { HrisMenuService } from './services/hris-menu.service';
-
+import { MatIconModule } from '@angular/material/icon';
+import { MatRippleModule } from '@angular/material/core';
 @NgModule({
   declarations: [HrisMenuComponent],
-  imports: [],
+  imports: [MatIconModule, MatRippleModule],
   exports: [HrisMenuComponent],
   providers: [HrisMenuService]
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HrisMenuModule } from 'projects/hris-menu/src/public-api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, HrisMenuModule
+    BrowserModule, HrisMenuModule, BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
