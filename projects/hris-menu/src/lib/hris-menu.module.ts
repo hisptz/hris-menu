08bf4c099bda969deeaf955fa44987ae76910e4e
Walter V. Faustine <waltervfaustine@gmail.com>
2020-07-16 15:53:26 +0300

    feat: 🎸 added ui support for searching apps from app list
    This commit will add ui support for searching apps from app list

--- projects/hris-menu/src/lib/hris-menu.module.ts
+++ projects/hris-menu/src/lib/hris-menu.module.ts
@@ -6,6 +6,7 @@ import { MatRippleModule } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { UserProfileComponent } from './components/user-profile/user-profile.component';
 import { CommonModule } from '@angular/common';
 import { ClickOutsideDirective } from './directives/click-outside.directive';
@@ -23,7 +24,8 @@ import { AppListComponent } from './components/app-list/app-list.component';
     MatRippleModule,
     MatCardModule,
     MatButtonModule,
-    MatProgressBarModule
+    MatProgressBarModule,
+    MatFormFieldModule
   ],
   exports: [HrisMenuComponent],
   providers: [HrisMenuService]
